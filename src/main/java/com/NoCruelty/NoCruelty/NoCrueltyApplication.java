d7ceabb0a1b57a5730be392d6264f0a6a608f852
JohanaMM <89425234+JohanaMM@users.noreply.github.com>
2022-01-06 22:59:20 -0300

    creacion de entidades

---
+++ src/main/java/com/NoCruelty/NoCruelty/NoCrueltyApplication.java
@@ -0,0 +1,13 @@
+package com.NoCruelty.NoCruelty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoCrueltyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoCrueltyApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/NoCruelty/NoCruelty/models/User.java
@@ -0,0 +1,107 @@
+package com.NoCruelty.NoCruelty.models;
+
+import io.orderup.DateAudit;
+import lombok.Data;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.Table;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User extends DateAudit implements UserDetails {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String surname;
+    private String password;
+    private String email;
+    private long phone;
+
+    @Enumerated(EnumType.STRING)
+    private Rol rol;
+
+    @ManyToOne(cascade = {CascadeType.MERGE, CascadeType.REMOVE})
+    private Zona zona;
+
+    public User() {
+    }
+    
+    public User(Long id, String name, String surname, String password, String email, long phone, Rol rol, Zona zona) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.password = password;
+        this.email = email;
+        this.phone = phone;
+        this.rol = rol;
+        this.zona = zona;
+    }
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public long getPhone() {
+        return phone;
+    }
+
+    public void setPhone(long phone) {
+        this.phone = phone;
+    }
+
+    public Rol getRol() {
+        return rol;
+    }
+
+    public void setRol(Rol rol) {
+        this.rol = rol;
+    }
+
+    public Zona getZona() {
+        return zona;
+    }
+
+    public void setZona(Zona zona) {
+        this.zona = zona;
+    }
