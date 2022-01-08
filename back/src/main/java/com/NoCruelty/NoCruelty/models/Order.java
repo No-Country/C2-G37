42687ca40115b355f10290bb42dbb2e15739a4c2
William Novoa <wjnovoam@ufpso.edu.co>
2022-01-08 12:25:05 -0500

    Creation of the entity order and product y enum category

--- back/src/main/java/com/NoCruelty/NoCruelty/models/Order.java
+++ back/src/main/java/com/NoCruelty/NoCruelty/models/Order.java
@@ -5,10 +5,28 @@
  */
 package com.NoCruelty.NoCruelty.models;
 
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.List;
+
 /**
  *
  * @author Usuario
  */
+@Entity
+@Data
 public class Order {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_order")
+    private Long idOrder;
+    private String comments;
+    @OneToMany(cascade = {CascadeType.MERGE, CascadeType.REMOVE})
+    private List<Product> productList;
+    @ManyToOne(cascade = {CascadeType.MERGE, CascadeType.REMOVE})
+    @JoinColumn(name = "id")
+    private User user;
+
 }
--- back/src/main/java/com/NoCruelty/NoCruelty/models/Product.java
+++ back/src/main/java/com/NoCruelty/NoCruelty/models/Product.java
@@ -5,10 +5,28 @@
  */
 package com.NoCruelty.NoCruelty.models;
 
+import com.NoCruelty.NoCruelty.enums.Category;
+import lombok.*;
+
+import javax.persistence.*;
+
 /**
  *
  * @author Usuario
  */
+@Entity
+@Data
 public class Product {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name  = "id_product")
+    private Long idProduct;
+    private String name;
+    private double price;
+    private String description;
+    private Category category;
+    private int stock;
+    private String urlImage;
+
 }
