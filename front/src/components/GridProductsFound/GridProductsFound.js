a797829f08e16f286da0dd5ff7530b762947488a
EliasYoc <eliyoc_97@hotmail.com>
2022-01-27 22:32:42 -0600

    correciones

--- front/src/components/GridProductsFound/GridProductsFound.js
+++ front/src/components/GridProductsFound/GridProductsFound.js
@@ -8,23 +8,15 @@ import {
 } from "../../features/products/productsSlice";
 import { useSelector } from "react-redux";
 const GridProductsFound = () => {
-  // const dispatch = useDispatch();
-  // const products = useSelector(selectProducts);
+  const dispatch = useDispatch();
+  const products = useSelector(selectProducts);
 
   useEffect(() => {
-    const fetchProducts = async () => {
-      const res = await fetch("http://34.133.133.181:8080/products");
-      console.log(res);
-      const data = await res.json();
-      console.log(data);
-    };
-    fetchProducts();
-
-    // dispatch(fetchProducts("http://localhost:3000/products"));
-  }, []);
+    dispatch(fetchProducts("http://34.133.133.181:8080/products"));
+  }, [dispatch]);
   return (
     <section className="grid-products">
-      {/* {products?.map((product, i) => (
+      {products?.map((product, i) => (
         <ProductFound
           key={product.id}
           img={product.urlImage}
@@ -32,7 +24,7 @@ const GridProductsFound = () => {
           price={product.price}
           altDetails={product.name}
         />
-      ))} */}
+      ))}
     </section>
   );
 };
