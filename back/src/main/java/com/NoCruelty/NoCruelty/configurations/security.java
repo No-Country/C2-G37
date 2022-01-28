666aeefeb09aee2176a3a05458f539d8870e9ed1
JohanaMM <89425234+JohanaMM@users.noreply.github.com>
2022-01-27 23:45:30 -0300

    change

--- back/src/main/java/com/NoCruelty/NoCruelty/configurations/security.java
+++ back/src/main/java/com/NoCruelty/NoCruelty/configurations/security.java
@@ -3,6 +3,7 @@ package com.NoCruelty.NoCruelty.configurations;
 import com.NoCruelty.NoCruelty.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -11,30 +12,30 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 //@Configuration
 //@EnableWebSecurity
 //@EnableGlobalMethodSecurity(prePostEnabled = true)
-public class security extends WebSecurityConfigurerAdapter{
+public class security extends WebSecurityConfigurerAdapter {
 
-	@Autowired
-	@Qualifier("userService")
-	public UserService userService;
-        
-        @Autowired
-        UserDetailsService  userDetailsService;
+//	@Autowired
+//	@Qualifier("userService")
+//	public UserService userService;
+    @Autowired
+    UserDetailsService userDetailsService;
 
-	@Override
+    @Override
 //	protected void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
 //		auth.userDetailsService(userService).passwordEncoder(new BCryptPasswordEncoder());
-        
-        protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(userDetailsService);
-	}
 
-        @Override
-	protected void configure(HttpSecurity http) throws Exception {
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
 //		http.headers().frameOptions().sameOrigin()
 //			.and()
 //				.authorizeRequests().antMatchers("/css/*","/.json/*", "/js/*", "/img/*", "*/*").permitAll()
@@ -51,11 +52,22 @@ public class security extends WebSecurityConfigurerAdapter{
 //			.and()
 //				.csrf().disable();
 //                
-                http.authorizeRequests()
-                        .antMatchers("/admin").hasRole("ROL_ADMIN,")
-                        .antMatchers("/user").hasRole("ROL_USER,")
-                        .antMatchers("/").permitAll()
-                        .and().formLogin();
-       
-	}	
+        http.authorizeRequests()
+                .antMatchers("/admin").hasRole("ROL_ADMIN,")
+                .antMatchers("/user").hasRole("ROL_USER,")
+                .antMatchers("/").permitAll()
+                .and().formLogin();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "PUT", "POST", "PATCH", "DELETE", "OPTIONS");
+            }
+        };
+    }
 }
