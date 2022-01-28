2951130884b4d0b3dc2b77e3d62a635d830e8947
JohanaMM <89425234+JohanaMM@users.noreply.github.com>
2022-01-27 23:32:29 -0300

    restcontroller

--- back/src/main/java/com/NoCruelty/NoCruelty/controllers/CardPaymentController.java
+++ back/src/main/java/com/NoCruelty/NoCruelty/controllers/CardPaymentController.java
@@ -7,12 +7,15 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST})
 @RequestMapping("/process_payment")
 public class CardPaymentController {
 
--- back/src/main/java/com/NoCruelty/NoCruelty/controllers/UserController.java
+++ back/src/main/java/com/NoCruelty/NoCruelty/controllers/UserController.java
@@ -11,12 +11,12 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-@Controller
+
+@RestController
+@CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST})
 @RequestMapping("/")
 //@RequestMapping("/user")
 public class UserController {
